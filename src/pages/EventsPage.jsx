13946748a7eba00956cc1597d05b60d75a2b75c0
LucasBonadeo <lucas@aliwenit.com>
2024-05-30 19:30:55 -0300

    InputSearch en pagina EventsPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,32 +1,77 @@
 import React, { useEffect, useState } from "react";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import { Container, Grid, Box } from "@mui/material/";
 import LocalDataBaseService from "../services/LocalDataBaseService";
 import MediaCard from "../components/Card";
+import Typography from "@mui/material/Typography";
+import { customMuiTheme } from "../config/customMuiTheme";
+import InputSearch from "../components/InputSearch"; // Asegúrate de importar el componente correcto
 
 export function EventsPage() {
+  const { contrastGreen } = customMuiTheme.colors;
   const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
   const localDBService = new LocalDataBaseService();
 
   useEffect(() => {
     const fetchEvents = async () => {
       const allEvents = await localDBService.getAllEvents();
       setEvents(allEvents);
+      setFilteredEvents(allEvents);
     };
     fetchEvents();
   }, []);
 
+  const handleSearch = (searchValue) => {
+    const lowercasedFilter = searchValue.toLowerCase();
+    const filteredData = events.filter((item) =>
+      item.name.toLowerCase().includes(lowercasedFilter)
+    );
+    setFilteredEvents(filteredData);
+  };
+
   return (
     <Container maxWidth="md">
       <div>
-        <h1>Soy la pagina de eventos</h1>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            justifyContent: "space-between",
+            alignItems: "center",
+            mb: 4,
+          }}
+        >
+          <Typography
+            variant="h2"
+            component="h2"
+            gutterBottom
+            sx={{
+              color: contrastGreen,
+              my: {
+                xs: 3,
+                md: 6,
+              },
+              fontSize: {
+                xs: "1.5rem",
+                md: "2rem",
+              },
+              alignSelf: "flex-start",
+            }}
+          >
+            Todos los eventos
+          </Typography>
+          <InputSearch
+            options={events.map((event) => event.name)}
+            onSearch={handleSearch}
+          />
+        </Box>
         <Grid container spacing={4}>
-          {events.map((event) => (
-            <Grid item xs={12} md={4} key={event.id}>
+          {filteredEvents.map((event) => (
+            <Grid item xs={12} md={6} lg={4} key={event.id}>
               <MediaCard
                 title={event.name}
                 artist={event.artist}
-                imageUrl={event.image} // Asegúrate de que los eventos tienen esta propiedad
+                imageUrl={event.image}
               />
             </Grid>
           ))}
